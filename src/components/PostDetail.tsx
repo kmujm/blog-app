65578580c963657c36cfb04b5a9e0a255e635105
kmujm <pmini09@naver.com>
2023-11-28 19:14:34 +0900

    feat: form component

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function PostDetail() {
   return (
     <>
@@ -11,7 +13,9 @@ export default function PostDetail() {
           </div>
           <div className="post__utils-box">
             <div className="post__delete">삭제</div>
-            <div className="post__edit">수정</div>
+            <div className="post__edit">
+              <Link to={`/posts/edit/1`}>수정</Link>
+            </div>
           </div>
           <div className="post__text">
             Lorem Ipsum is simply dummy text of the printing and typesetting
--- src/pages/posts/new.tsx
+++ src/pages/posts/new.tsx
@@ -1,12 +1,11 @@
-import Footer from "components/Footer";
 import Header from "components/Header";
+import PostForm from "components/PostForm";
 
 export default function PostsNew() {
   return (
     <>
       <Header />
-      <div> Post Form</div>
-      <Footer />
+      <PostForm />
     </>
   );
 }
