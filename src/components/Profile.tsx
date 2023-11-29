97cd9395a24a7d3e438e0504e0d771c060600e45
kmujm <pmini09@naver.com>
2023-11-29 14:08:36 +0900

    feat: context로 user 관리하기

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
-
 import { getAuth, signOut } from "firebase/auth";
 import { app } from "firebaseApp";
 
 import { toast } from "react-toastify";
+import { useContext } from "react";
+import AuthContext from "context/AuthContext";
 
 export default function Profile() {
-  const auth = getAuth(app);
+  const { user } = useContext(AuthContext);
 
   const onSignOut = async () => {
     try {
@@ -27,10 +27,8 @@ export default function Profile() {
         <div className="flex__box--lg">
           <div className="profile__image" />
           <div>
-            <div className="profile__email">{auth?.currentUser?.email}</div>
-            <div className="profile__name">
-              {auth?.currentUser?.displayName || "사용자"}
-            </div>
+            <div className="profile__email">{user?.email}</div>
+            <div className="profile__name">{user?.displayName || "사용자"}</div>
           </div>
         </div>
         {/* role 속성 : 더 명확한 html요소 구조 의미 부여 */}
